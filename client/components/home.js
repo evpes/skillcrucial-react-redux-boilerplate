eff9c96c54bb2bb11431b0dd802524094f822145
Evgeny Pesterev <pistarsan@gmail.com>
2020-08-29 09:17:30 +0500

    homework p1

--- client/components/home.js
+++ client/components/home.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import Head from './head'
+
 // import wave from '../assets/images/wave.jpg'
 
 const Home = () => {
@@ -8,11 +10,20 @@ const Home = () => {
   return (
     <div>
       <Head title="Hello" />
-      <img alt="wave" src="images/wave.jpg" />
-      <button type="button" onClick={() => setCounterNew(counter + 1)}>
+      <div className="flex items-center justify-center h-screen">
+      <button className="bg-indigo-800 hover:text-red-500 text-white font-bold rounded-lg border shadow-lg p-10" type="button" onClick={() => setCounterNew(0)}>
+        resetCounter
+      </button>
+      <button className="bg-indigo-800 hover:text-red-500 text-white font-bold rounded-lg border shadow-lg p-10" type="button" onClick={() => setCounterNew(counter + 1)}>
         updateCounter
       </button>
-      <div> Hello World Dashboard {counter} </div>
+        <div className="bg-indigo-800 hover:text-red-500 text-white font-bold rounded-lg border shadow-lg p-10">
+        Counter {counter}
+        </div>
+        <div className="bg-indigo-800 hover:text-red-500 text-white font-bold rounded-lg border shadow-lg p-10">
+        <Link to="/">Go to root</Link>
+        </div>
+      </div>
     </div>
   )
 }
--- server/server.js
+++ server/server.js
@@ -5,10 +5,12 @@ import bodyParser from 'body-parser'
 import sockjs from 'sockjs'
 import { renderToStaticNodeStream } from 'react-dom/server'
 import React from 'react'
-
+import axios from 'axios'
 import cookieParser from 'cookie-parser'
 import config from './config'
 import Html from '../client/html'
+
+const { readFile, writeFile, unlink } = require("fs").promises
 
 const Root = () => ''
 
@@ -41,10 +43,111 @@ const middleware = [
 
 middleware.forEach((it) => server.use(it))
 
+/*
+server.get('/api/v1/users', async (req, res) => {
+  const { data: users } = await axios('https://jsonplaceholder.typicode.com/users')
+  res.json({users})
+})
+*/
+
+/*
+server.get('/api/v1/users/:name', (req, res) => {
+  const { name } = req.params
+  res.json({ name})
+})
+*/
+
+server.get('/api/v1/users', (req, res) => {
+   readFile(`${__dirname}/test.json`, { encoding: "utf8" })
+   .then(usersText => {
+     res.json( JSON.parse(usersText) )
+   })
+   .catch(err => {
+     console.log(err)
+    axios('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.data)
+    .then(data => {
+      writeFile(`${__dirname}/test.json`, JSON.stringify(data), { encoding: "utf8" })
+        res.json ( data )
+    })
+   })
+
+})
+
+server.post('/api/v1/users', (req, res) => {
+  readFile(`${__dirname}/test.json`, { encoding: "utf8" })
+  .then(usersInfo => {
+    let ob = JSON.parse(usersInfo)
+    const newId = Math.max.apply(0 ,ob.reduce((acc, rec) => {
+      return [...acc, rec.id]
+    }, [])) + 1
+    ob = [...ob, {id: newId }]
+    writeFile(`${__dirname}/test.json`, JSON.stringify(ob), { encoding: "utf8" })
+    res.json( {status: 'success', id: newId} )
+  })
+  .catch(err => {
+    res.json( {status: 'failed', error: err} )
+    console.log(err)
+  })
+})
+
+server.patch('/api/v1/users/:userid', (req, res) => {
+  const { userid } = req.params
+  const  update  = req.body
+  readFile(`${__dirname}/test.json`, { encoding: "utf8" })
+  .then(users => {
+    let ob = JSON.parse(users)
+    ob = ob.reduce((acc, rec) => {
+      if (rec.id === Number(userid) ) {
+        return [...acc, {...rec, ...update}]
+      }
+      return  [...acc, rec]
+    }, [])
+    writeFile(`${__dirname}/test.json`, JSON.stringify(ob), { encoding: "utf8" })
+    res.json( {status: 'success', id: userid} )
+  })
+  .catch(err => {
+    res.json( {status: 'failed', error: err} )
+    console.log(err)
+  })
+})
+
+server.delete('/api/v1/users/:userid', (req, res) => {
+  const { userid } = req.params
+  readFile(`${__dirname}/test.json`, {encoding: "utf8"})
+  .then(usr => {
+    let ob = JSON.parse(usr)
+    ob = ob.reduce((acc,rec) => {
+      if (rec.id === Number(userid)) {
+        return [acc]
+      }
+      return [...acc, rec]
+     },[])
+     writeFile(`${__dirname}/test.json`, JSON.stringify(ob), { encoding: "utf8"})
+     res.json({status: "success", id: userid})
+  })
+  .catch(err => {
+    res.json({status: "failed", error: err})
+  })
+})
+
+server.delete('/api/v1/users', (req, res) => {
+  unlink(`${__dirname}/test.json`)
+  .then( () => {
+    res.json({status: "success"})
+  })
+  .catch(err => {
+    res.json({status: "failed", error: err})
+  })
+})
+
 server.use('/api/', (req, res) => {
   res.status(404)
   res.end()
 })
+
+
+
 
 const [htmlStart, htmlEnd] = Html({
   body: 'separator',
